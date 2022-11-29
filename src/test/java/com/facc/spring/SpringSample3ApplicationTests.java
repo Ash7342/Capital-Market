30bc78c2026e8ffb6e781a3b7bd66dae439d342c
Asus <Asus@DESKTOP-QNHG91P>
2022-11-29 19:38:27 +0530

    Project Manager creating local repository

---
+++ src/test/java/com/facc/spring/SpringSample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
